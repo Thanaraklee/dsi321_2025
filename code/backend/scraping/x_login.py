5f6d0c76e057db24ecf9adcd573a91c79e35cd58
thanaraklee <bzank.lee@hotmail.com>
2025-05-03 16:03:50 +0700

    FIX: Fix no directory when save auth file.

--- code/backend/scraping/x_login.py
+++ code/backend/scraping/x_login.py
@@ -1,4 +1,16 @@
 from playwright.sync_api import sync_playwright
+from rich.prompt import Prompt
+from rich.text import Text
+from pathlib import Path
+import json
+import os
+# Import modern logging configuration
+from config.logging.modern_log import LoggingConfig
+# Import path configuration
+from config.path_config import AUTH_TWITTER
+
+
+logger = LoggingConfig(level="DEBUG").get_logger()
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)  # headless=False so you can log in manually
@@ -6,7 +18,20 @@ with sync_playwright() as p:
     page = context.new_page()
     page.goto("https://x.com/login")
 
-    input("Log in to Twitter manually, then press Enter here...")
+    logger.info("Page loaded. Waiting for login...")
+    Prompt.ask(Text("Log in to Twitter manually, then press Enter here...", style="bold green"))
 
-    context.storage_state(path="twitter_auth.json")  # Save cookies/session
+    # Create the directory if it doesn't exist
+    Path(AUTH_TWITTER).parent.mkdir(parents=True, exist_ok=True)
+
+    context.storage_state(path=AUTH_TWITTER)  # Save cookies/session
     browser.close()
+
+    # Validate the saved session
+    logger.info("Session saved. Validating...")
+    data = json.load(open(AUTH_TWITTER, "r"))
+    if len(data['cookies']) > (13*2) and len(data['cookies'][0]['value']) > 500:
+        logger.info("Session is valid ✅")
+    else:
+        logger.error("Session is invalid ❌. Removing file...")
+        os.remove(AUTH_TWITTER)
